d94bf04b06ac0c71b6647a2994e15da899176d36
Alek <sforaaleksander@gmail.com>
2020-07-02 18:04:44 +0200

    create styling, html structure, js adding notes functionality

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let noteId = 0;
+
+function createNote() {
+    const divBox = document.createElement("div");
+    divBox.setAttribute("id", "divBox " + noteId.toString());
+    divBox.setAttribute("class", "divBox");
+
+    const titleBox = document.createElement("div");
+    titleBox.setAttribute("class", "titleBox");
+    const header = document.createElement("h4");
+    header.setAttribute("class", "noteHeader");
+    header.setAttribute("contenteditable", "true");
+
+    header.innerHTML = "Edit your note's title!";
+    const text = document.createElement("textarea");
+    text.setAttribute("class", "text");
+    text.innerHTML = "Write your note!";
+
+    divBox.appendChild(titleBox);
+    divBox.appendChild(header);
+    divBox.appendChild(text);
+
+
+    const container = document.getElementById("container");
+
+    container.appendChild(divBox);
+    noteId++;
+    console.log("note added");
+}
+
+function main(){
+    document.getElementById("new-note-btn").addEventListener('click', createNote);
+}
+
+main();
