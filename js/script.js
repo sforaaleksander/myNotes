1b99c4a4400e4a5e23b1afd387c573f877a38931
Alek <sforaaleksander@gmail.com>
2020-07-03 13:37:51 +0200

    split createNote() functionality to seperate functions

--- js/script.js
+++ js/script.js
@@ -1,20 +1,38 @@
 let noteId = 0;
 
-function createNote() {
+function createDivBox() {
     const divBox = document.createElement("div");
     divBox.setAttribute("id", "divBox " + noteId.toString());
     divBox.setAttribute("class", "divBox");
+    return divBox;
+}
 
+function createTitleBox() {
     const titleBox = document.createElement("div");
     titleBox.setAttribute("class", "titleBox");
+    return titleBox;
+}
+
+function createHeader() {
     const header = document.createElement("textarea");
     header.setAttribute("class", "noteHeader");
     header.setAttribute("contenteditable", "true");
-
     header.innerHTML = "Edit your note's title!";
+    return header;
+}
+
+function createText() {
     const text = document.createElement("textarea");
     text.setAttribute("class", "text");
     text.innerHTML = "Write your note!";
+    return text;
+}
+
+function createNote() {
+    const divBox = createDivBox();
+    const titleBox = createTitleBox();
+    const header = createHeader();
+    const text = createText();
 
     titleBox.appendChild(header);
     divBox.appendChild(titleBox);
